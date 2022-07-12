c7c522a7a4c0a39ad6e6e1a20e069d876c3eb843
m0nds <elegbede.raymond@gmail.com>
2022-07-12 23:31:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {data} from './data.js'
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const {id, img, name, role, description} = data[count]
+
+  const checkNumbers =(numbers)=> {
+    if(numbers > data.length -1){
+      return 0
+    }
+    if(numbers < 0){
+      return data.length -1
+    }
+    return numbers
+  }
+
+  const nextBtn = () => {
+    setCount((count)=> {
+      let newCount = count + 1
+      return checkNumbers(newCount)
+    })
+  }
+
+  const prevBtn = () => {
+    setCount((count)=> {
+      let newCount = count - 1
+      return checkNumbers(newCount)
+    })
+  }
+
+  const randomNumbers = () => {
+    let randomPeople = Math.floor(Math.random() * data.length)
+    if(randomPeople === count){
+      return randomPeople = count + 1
+    }
+    setCount(checkNumbers(randomPeople))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h2>Our Reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="reviews" key={id}>
+        <img src={img} alt=""/>
+        <h4>{name}</h4>
+        <span>{role}</span>
+        <p>{description}</p>
+        <div className="btns">
+          <input onClick={prevBtn} type="submit" value="<"/>
+          <input onClick={nextBtn} type="submit" value=">"/>
+        </div>
+        <button className="submit" onClick={randomNumbers}>Surprise me</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1581403341630-a6e0b9d2d257?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      name: "Susan belle",
+      role: "Frontend developer",
+      description: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGUlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      name: "Henry Karl",
+      role: "Backend developer",
+      description: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1524660988542-c440de9c0fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fHByb2ZpbGUlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      name: "Alex funn",
+      role: "Product designer",
+      description: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+    {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHByb2ZpbGUlMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      name: "Howard Flinn",
+      role: "CEO",
+      description: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    }
+  ]
